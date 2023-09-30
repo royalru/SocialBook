061ebb1214c452b60144e7445500fa27b90a39af
royalru <abujahidroyal@gmail.com>
2023-10-01 03:21:15 +0600

    update

---
+++ ds.js
@@ -0,0 +1,22 @@
+let arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+
+function findSumPair(arr){
+    let left=0;
+    let right=arr.length-1;
+
+    while(left<right){
+        sum=arr[left]+arr[right]
+        if(sum==0){
+            return [arr[left], arr[right]];
+        }
+        else if(sum>0){
+            right--;
+        }
+        else{
+            left++;
+        }
+    }
+}
+  
+const result = findSumPair([-5, -4, -3, -2, 0, 2, 4, 6, 8]);
+console.log(result);
